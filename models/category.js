cbd98659b5b9a8ff77097a35260c743f26ce6085
Cartoon <cartoonshow57@gmail.com>
2020-10-02 11:51:12 +0530

    Added Category Schema

---
+++ models/category.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var CategorySchema = new Schema(
+    {
+        title: { type: String, required: true }
+    }
+);
+
+CategorySchema.virtual('url').get(function() {
+    return '/catalog/category/' + this._id;
+});
+
+module.exports = mongoose.model('Category', CategorySchema);
