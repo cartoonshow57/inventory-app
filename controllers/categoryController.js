cdc5fe74c6567e1337048ec3022120a1f1e0632a
Cartoon <cartoonshow57@gmail.com>
2020-10-16 15:58:59 +0530

    Created layout

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -2,5 +2,5 @@ var Category = require('../models/category.js');
 
 
 exports.category_list = function(req, res) {
-    res.send('NOT IMPLEMENTED: category list');
+    res.render('../views/index.pug');
 };
