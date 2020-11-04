0f86829267febc37fb35a14bca08137f9cd4f426
Cartoon <cartoonshow57@gmail.com>
2020-11-04 23:45:03 +0530

    Created Utility Create Form

--- controllers/utilityController.js
+++ controllers/utilityController.js
@@ -1,13 +1,15 @@
 var Utility = require('../models/utility');
 var Category = require('../models/category');
+const { body, validationResult } = require('express-validator');
 const { nextTick } = require('async');
+const category = require('../models/category');
 
 
 exports.utility_list = function(req, res) {
-    Category.find({}, 'title image')
+    Category.find({ type: 'Utility' })
         .exec(function(err, utility_list) {
             if (err) { return next(err); }
-            res.render('utility_list', { title: 'Utility List', list_utility: utility_list.slice(5, 8) });
+            res.render('utility_list', { title: 'Utility List', list_utility: utility_list });
         });
 };
 
@@ -18,18 +20,50 @@ exports.utility_detail = function(req, res) {
                 populate('skins').
                 exec(function(err, utility_list) {
                     if (err) { return next(err); }
-                    res.render('utility_detail', { title: results.title + ' Skins', skins_list: utility_list });
+                    if (typeof utility_list !== 'undefined' && utility_list.length > 0) {
+                        res.render('utility_detail', { title: results.title + ' Skins', skins_list: utility_list });
+                    }
+                    else {
+                        res.render('utility_detail', { title: results.title + ' Skins'});
+                    }
                 });
         });
 };
 
 exports.utility_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: utility create GET');
+    res.render('utility_form', { title: 'Create New Utility'});
 };
 
-exports.utility_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: utility create POST');
-};
+exports.utility_create_post = [
+    body('name', 'Utility name must be specified.').trim().isLength({ min: 1 }).escape(),
+    (req, res, next) => {
+        const errors = validationResult(req);
+        var utility = new Category({
+            type: 'Utility',
+            title: req.body.name,
+            image: '/images/' + req.body.image
+        });
+        if (!errors.isEmpty()) {
+            res.render('utility_form', { title: 'Create New Utility', errors: errors.array() });
+            return;
+        }
+        else {
+            category.findOne({ title: req.body.name }).
+                exec(function(err, found_utility) {
+                    if (err) { return next(err); }
+                    if (found_utility) {
+                        res.redirect(found_utility.utility_url);
+                    }
+                    else {
+                        utility.save(function(err) {
+                            if (err) { return next(err); }
+                            res.redirect(utility.utility_url);
+                        });
+                    }
+                });
+        }
+    }
+];
 
 exports.utility_delete_get = function(req, res) {
     res.send('NOT IMPLEMENTED: utility delete GET');
